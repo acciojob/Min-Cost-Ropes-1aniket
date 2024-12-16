93626a6aa82f22d3d99790913c22ddcd327891bf
Aniket Bhogawar <39596152+1aniket@users.noreply.github.com>
2024-12-16 19:55:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+	arr.sort();
+
+	let ans =0;
+
+  
+	  
+	for(let i=0;i<arr.length-1;i++)
+	{
+	 
+	  let temp=(arr[i]+arr[i+1]);
+		ans+= (arr[i]+arr[i+1]);
+		arr[i+1]=temp;
+		
+	}
+  
+    
+	   return ans;
   
 }
 
